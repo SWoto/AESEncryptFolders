8632b35f5f711fe037b5d2d863c04d4758a3297e
SWoto <brunostinghel137@gmail.com>
2020-10-15 14:09:06 -0300

    first commit

---
+++ EncryptFolders.py
@@ -0,0 +1,137 @@
+# get_random_password_string
+import random
+import string
+
+# find the paths that needed to be encrypted
+from os import listdir, remove, path
+from os.path import isdir, join, isfile
+
+# to run the commands
+import subprocess
+
+class AESEncryptFolders:
+    """
+    This object will get the folders with some signal in thier name, like (D_ENC), 
+    zip them, and encrypt it using the generated password.
+    After encrypting it, the zip will be deleted and the password returned so you 
+    can write it down somewhere safe.
+    """
+    def __init__(self, KEY_SIZE, PATH_TO_FOLDERS, PATH_TO_DEST, FOLDERS_WITH, INTER = 20000):
+        self.key_size = KEY_SIZE
+        self.path_to_folders = PATH_TO_FOLDERS
+        self.path_to_dest = PATH_TO_DEST
+        self.folders_with = FOLDERS_WITH
+        self.password = self.random_password_string().replace("'",'*')
+        self.inter = INTER
+
+    def random_password_string(self):
+        """
+        Generates a password with length of key_size 
+        """
+        password_characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(password_characters) for i in range(self.key_size))
+        return password
+
+    @staticmethod
+    def ecape_characters(name):
+        """
+        Escape characters so we can use them as paths 
+        """
+        return name.replace('(','\(').replace(')','\)').replace(' ','\ ')
+
+    @staticmethod
+    def subprocess_shell_command(cmd, path=None):
+        """
+        Used to run shell comands 
+        """
+        sp = subprocess.Popen(cmd,
+                cwd=path,
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+        rc=sp.wait()
+        out,err=sp.communicate()
+
+        if err:
+            print('ERROR: A problem ocurred when using subprocess')
+            print('Return Code:',rc)
+            print('output is: \n', out)
+            print('error is: \n', err)
+
+        return err
+
+    def zip_folder(self, folder_path, folders_name, destination_path = None):
+        """
+        Zip folder and its subfolders 
+        """
+        if not(destination_path):
+            destination_path = folder_path
+
+        folder_path = self.ecape_characters(folder_path)
+        folder_name = self.ecape_characters(folders_name)
+        destination_path = self.ecape_characters(destination_path)
+
+        cmd = 'zip -9 -r -q {name}.zip {source}'.format(
+                name=destination_path+folder_name, 
+                source=folder_name)
+
+        err = self.subprocess_shell_command(cmd, folder_path)
+        
+        if not(err):
+            print("\tFolder {} zipped".format(folder_path+folder_name))
+
+    def zip_folders(self, folder_path, folders_name, destination_path = None):
+        """
+        Zip multiple folders and its subfolders 
+        """
+        if isinstance(folders_name, list): 
+            for folder_name in folders_name:
+                self.zip_folder(folder_path, folder_name, destination_path)
+        else: 
+            folder_name=folders_name
+            self.zip_folder(folder_path, folder_name, destination_path)
+
+    def encrypt(self):
+        """
+        Encrypt zip folder using AES256 from openssl library 
+        """
+        folders = [f for f in listdir(self.path_to_folders) if isdir(join(self.path_to_folders, f)) and self.folders_with in f]
+        print('Folders to be encrypted: {}'.format(folders))
+        
+        for folder in folders:
+            self.zip_folders(self.path_to_folders, folder)
+
+            folder_aux = folder
+            folder_aux = folder_aux.replace(self.folders_with, '(ENC)')
+            enc_full_path = self.ecape_characters(self.path_to_dest + folder_aux + '.enc')
+            full_path_folder = self.ecape_characters(self.path_to_folders + folder + '.zip')
+
+            cmd = "openssl enc -aes-256-cbc -pbkdf2 -md sha512 -iter {inter} -in {target} -out {destination} -k '{password}'".format(
+                password=self.password, target=full_path_folder, destination=enc_full_path, inter=self.inter)
+
+            err = self.subprocess_shell_command(cmd, self.path_to_folders)
+        
+            if not(err):
+                print("\tFolder {} encrypted".format(self.path_to_folders + folder))
+                remove(join(self.path_to_folders, folder+'.zip'))
+
+        return self.password
+
+    def decrypt(self, password):
+        """
+        Decript .enc folder with openssl library 
+        """
+        folders = [f for f in listdir(self.path_to_dest) if isfile(join(self.path_to_dest, f)) and '(ENC)' in f]
+        print('Folders to be decrypted: {}'.format(folders))
+        
+        for folder in folders:
+            enc_full_path = self.ecape_characters(self.path_to_dest + folder)
+            full_path_folder = self.ecape_characters(self.path_to_folders + folder.replace('.enc','.zip').replace('(ENC)', self.folders_with))
+
+            cmd = "openssl enc -d -aes-256-cbc -pbkdf2 -md sha512 -iter {inter} -in {target} -out {destination} -k '{password}'".format(
+                password=password, target=enc_full_path, destination=full_path_folder, inter=self.inter)
+
+            err = self.subprocess_shell_command(cmd, self.path_to_folders)
+        
+            if not(err):
+                print("\tFolder {} decrypted".format(self.path_to_folders + folder))
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # AESEncryptFolders
-zips folder and encrypt it using AES256 (openssl)
+This library was created to encrypt some folders within a directory using AES256 encryption from [OpenSSL](https://www.openssl.org). 
+
+
+# Table of Contents
+- [How to Use](#how-to-use)
+- [OS](#os)
+
+# How to Use
+
+There are two ways to use it:
+1. To encrypt a folder: 
+    - search for all folders with specif flag in their names
+    - zip each one of them
+    - encrypt it changing the flag to `(ENC)`
+    - delete the created zip
+
+2. To decrypt a folder: 
+    - search for all files with `(ENC)` in their names and
+    - decrypt it changing the flag to the old one (you need to specify it)
+    - move the zip to the input folder
+
+We will give tou an example showing both at the same time, and you can use the examples folder in this repository, just remmember to change its full path according to where you place it.
+
+```python
+from EncryptFolders import AESEncryptFolders
+
+KEY_SIZE = 32
+PATH_TO_FOLDERS = '/Users/username/Documents/testing/input/'
+FOLDERS_WITH = '(D_ENC)' # will search for this in the folders name
+ENC_PATH = '/Users/username/Documents/testing/output/' # you can change if you want
+
+test = AESEncryptFolders(KEY_SIZE, PATH_TO_FOLDERS, ENC_PATH, FOLDERS_WITH)
+password = test.encrypt()
+test.decrypt(password)
+```
+
+**Output:**
+```sh
+Folders to be encrypted: ['(D_ENC) folder', '(D_ENC) folder2']
+        Folder /Users/username/Documents/testing/input/\(D_ENC\)\ folder zipped
+        Folder /Users/username/Documents/testing/input/(D_ENC) folder encrypted
+        Folder /Users/username/Documents/testing/input/\(D_ENC\)\ folder2 zipped
+        Folder /Users/username/Documents/testing/input/(D_ENC) folder2 encrypted
+Folders to be decrypted: ['(ENC) folder2.enc', '(ENC) folder.enc']
+        Folder /Users/username/Documents/testing/input/(ENC) folder2.enc decrypted
+        Folder /Users/username/Documents/testing/input/(ENC) folder.enc decrypted
+```
+
+# OS
+This library was tested in a OSX 10.15 and i strongly believe that it will work with linux. 
+It will probably crash in Windows due to the ecape_characters function, thought is easy to fix this.
